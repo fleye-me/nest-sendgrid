ae4425fe418ae0c03debc00ae2db240430db0178
Joao Schaa <joao.schaab@gmail.com>
2019-09-02 15:11:01 -0300

    Bump...

--- src/sendgrid.service.ts
+++ src/sendgrid.service.ts
@@ -38,13 +38,14 @@ export class SendGridService {
       companyName: 'EMrpesa 1',
       signal: '50%',
       battery: 'bateria',
+      statusColor: '#ffd33d',
       isRfOn: 'Ligado',
     };
     const output = Mustache.render(template, data);
     console.log(output);
     try {
       return await Sendgrid.send({
-        to,
+        to: 'joao.schaab@sthima.com.br',
         from: this.sendGridConfig.sendgridEmailFrom,
         subject,
         html: output,
