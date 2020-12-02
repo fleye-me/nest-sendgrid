eff519e7e0c0c093e13faaade37804f037df65e1
Rodrigo Chamun <rodrigo@door2door.io>
2020-12-01 23:06:35 -0300

    Bump version to 1.0.1

--- dist/sendgrid.service.d.ts
+++ dist/sendgrid.service.d.ts
@@ -1,9 +1,10 @@
+/// <reference types="request" />
 import { SendGridConfig } from './interfaces/email.interface';
 import { Attachment } from './attachment';
 export declare class SendGridService {
     private readonly sendGridConfig;
     constructor(sendGridConfig: SendGridConfig);
-    sendMail(to: string, subject: string, html: string, attachments?: Attachment[]): Promise<[any, {}]>;
-    renderAndSendMail(to: string, subject: string, templatePath: string, data: any, attachments?: Attachment[]): Promise<[any, {}]>;
+    sendMail(to: string, subject: string, html: string, attachments?: Attachment[]): Promise<[import("request").Response, {}]>;
+    renderAndSendMail(to: string, subject: string, templatePath: string, data: any, attachments?: Attachment[]): Promise<[import("request").Response, {}]>;
     private send;
 }
