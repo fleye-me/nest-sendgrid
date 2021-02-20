7b5e9a2afe151a158da898e57ee5b38747e693aa
Diorgeles <diorgelesdias@gmail.com>
2021-02-19 21:20:45 -0300

    fix: fixed repsonse error in send mail

--- src/sendgrid.service.ts
+++ src/sendgrid.service.ts
@@ -1,36 +1,51 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { SENDGRID_CONFIG } from './sendgrid.constants';
-import { SendGridConfig } from './interfaces/email.interface';
-import * as Sendgrid from '@sendgrid/mail';
-import * as Mustache from 'mustache';
-import { readFileSync } from 'fs';
-import { Attachment } from './attachment';
-
+import { Injectable, Inject } from "@nestjs/common";
+import { SENDGRID_CONFIG } from "./sendgrid.constants";
+import { SendGridConfig } from "./interfaces/email.interface";
+import * as Sendgrid from "@sendgrid/mail";
+import * as Mustache from "mustache";
+import { readFileSync } from "fs";
+import { Attachment } from "./attachment";
 
 @Injectable()
 export class SendGridService {
   constructor(
-    @Inject(SENDGRID_CONFIG) private readonly sendGridConfig: SendGridConfig,
+    @Inject(SENDGRID_CONFIG) private readonly sendGridConfig: SendGridConfig
   ) {
     Sendgrid.setApiKey(this.sendGridConfig.sendgridApiKey);
   }
 
-  async sendMail(to: string, subject: string, html: string, attachments?: Attachment[]) {
+  async sendMail(
+    to: string,
+    subject: string,
+    html: string,
+    attachments?: Attachment[]
+  ) {
     return await this.send(to, subject, html, attachments);
   }
 
-  async renderAndSendMail(to: string, subject: string, templatePath: string, data: any, attachments?: Attachment[]) {
-    const template = readFileSync(templatePath, 'utf8');
+  async renderAndSendMail(
+    to: string,
+    subject: string,
+    templatePath: string,
+    data: any,
+    attachments?: Attachment[]
+  ) {
+    const template = readFileSync(templatePath, "utf8");
     const output = Mustache.render(template, data);
     return await this.send(to, subject, output, attachments);
   }
 
-  private async send(to: string, subject: string, html: string, attachments: Attachment[] = []) {
-    if(this.sendGridConfig.devOptions) {
+  private async send(
+    to: string,
+    subject: string,
+    html: string,
+    attachments: Attachment[] = []
+  ) {
+    if (this.sendGridConfig.devOptions) {
       if (this.sendGridConfig.devOptions.disableSend) {
         return;
       }
-      if(this.sendGridConfig.devOptions.defaultDestinyAddress) {
+      if (this.sendGridConfig.devOptions.defaultDestinyAddress) {
         to = this.sendGridConfig.devOptions.defaultDestinyAddress;
       }
     }
@@ -40,11 +55,10 @@ export class SendGridService {
         from: this.sendGridConfig.sendgridEmailFrom,
         subject,
         html,
-        attachments: attachments.map(a => a.toObject())
+        attachments: attachments.map((a) => a.toObject()),
       });
     } catch (error) {
-      console.log(error);
+      return new Error(error);
     }
   }
-
 }
